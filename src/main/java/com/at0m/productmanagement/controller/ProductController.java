df7b8cd4d3346863f79c90831cab0b2a5b498d68
Girishu M S <girishums1999@gmail.com>
2022-12-19 19:25:58 +0530

    Implemented config server

--- src/main/java/com/at0m/productmanagement/controller/ProductController.java
+++ src/main/java/com/at0m/productmanagement/controller/ProductController.java
@@ -31,7 +31,6 @@ public class ProductController {
 
     @GetMapping("/products")
     public List<ProductResponseResource> getAllProductsList(){
-        //List<AvailableQuantity> quantityList = consumer.getAllQuantity();
         return productService.getAllProductsList();
     }
 
